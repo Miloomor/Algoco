5e78a7514d66b3a75efaba1c47dcc1e12a569815
Miloomor <miloomor@users.noreply.github.com>
2026-04-08 00:03:00 -0400

    Primer commit

---
+++ code/matrix_multiplication/algorithms/naive.cpp
@@ -0,0 +1,57 @@
+// Fuente GeeksforGeeks: Strassen's Matrix Multiplication, Aproach 1
+// URL: https://www.geeksforgeeks.org/dsa/strassens-matrix-multiplication/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> multiply(vector<vector<int>> &mat1, vector<vector<int>> &mat2) {
+    
+    int n = mat1.size(), m = mat1[0].size(), 
+                                    q = mat2[0].size();        
+
+    // Initialize the result matrix with 
+    // dimensions n×q, filled with 0s
+    vector<vector<int>> res(n, vector<int>(q, 0));
+
+    // Loop through each row of mat1
+    for (int i = 0; i < n; i++) {
+        
+        // Loop through each column of mat2
+        for (int j = 0; j < q; j++) {
+            
+            // Compute the dot product of 
+            // row mat1[i] and column mat2[][j]
+            for (int k = 0; k < m; k++) {
+                res[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    vector<vector<int>> mat1 = {
+        {1, 2, 3},
+        {4, 5, 6}
+    }; 
+
+    vector<vector<int>> mat2 = {
+        {7, 8},
+        {9, 10},
+        {11, 12}
+    };
+
+    vector<vector<int>> res = multiply(mat1, mat2); 
+
+    // Print the resulting matrix
+    for (int i = 0; i < res.size(); i++) {
+        for (int j = 0; j < res[i].size(); j++) {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
